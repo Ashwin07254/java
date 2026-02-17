5d2620bf0ab2c0a1b1368595e612d0f6e3aa27fc
Ashwin07254 <ashwinpai75005@gmail.com>
2026-02-17 09:55:02 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-public class HelloWorld
-{
-public static void main(String args[])
-{
-System.out.println("HELLO WORLD!!");
-}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World"); // Prints the message to the console
+    }
 }
